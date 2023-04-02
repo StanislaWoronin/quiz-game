8c90394627129db7b63c9f53b885499739a0d965
Stanislav <stanislaworonin@gmail.com>
2023-04-03 00:10:51 +0300

    start new hw 25

--- src/modules/public/pair-quiz-game/infrastructure/sql/quiz-game-query.repository.ts
+++ src/modules/public/pair-quiz-game/infrastructure/sql/quiz-game-query.repository.ts
@@ -14,29 +14,23 @@ export class QuizGameQueryRepository implements IQuizGameQueryRepository {
 
   async getMyGames(userId): Promise<ViewPage<ViewGame>> {
     const query = `
-    SELECT 
-    g.id, 
-    g.status, 
-    g."pairCreatedDate", 
-    g."startGameDate", 
-    g."finishGameDate",
-    COALESCE(gp1."gameHost", 'false') AS "firstUserHost",
-    fp.score AS "firstPlayerScore",
-    sp.score AS "secondPlayerScore",
-JSON_BUILD_OBJECT('id', fp."userId", 'login', fu.login) AS "firstUser",
-JSON_BUILD_OBJECT('id', sp."userId", 'login', su.login) AS "secondUser",
-JSON_BUILD_OBJECT('id', gq."questionId", 'body', q.body) AS questions,
-    JSON_BUILD_OBJECT('answerStatus', ua."answerStatus", 'addedAt', ua."addedAt", 'questionId', gq."questionId") AS answer
-FROM 
-    sql_game g
-    JOIN sql_game_progress fp ON g.id = fp."gameId" AND fp."userId" = 'c9b93818-e50a-42c0-ba27-6af2c6e13c49'
-    JOIN sql_game_progress sp ON g.id = sp."gameId" AND sp."userId" != 'c9b93818-e50a-42c0-ba27-6af2c6e13c49'
-    JOIN sql_game_questions gq ON gq."gameId" = g.id
-    LEFT JOIN sql_questions q ON q.id = gq."questionId"
-    JOIN sql_users fu ON fp."userId" = fu.id
-    JOIN sql_users su ON sp."userId" = su.id
-    LEFT JOIN sql_game_progress gp1 ON fp."userId" = gp1."userId" AND g.id = gp1."gameId"
-    JOIN sql_user_answer ua ON ua."questionId" = gq."questionId" AND ua."gameId" = g.id
+        SELECT g.id, g.status, g."pairCreatedDate", g."startGameDate", g."finishGameDate",
+               COALESCE(gp1."gameHost", 'false') AS "firstUserHost",
+               fp.score AS "firstPlayerScore",
+               sp.score AS "secondPlayerScore",
+               JSON_BUILD_OBJECT('id', fp."userId", 'login', fu.login) AS "firstUser",
+               JSON_BUILD_OBJECT('id', sp."userId", 'login', su.login) AS "secondUser",
+               JSON_BUILD_OBJECT('id', gq."questionId", 'body', q.body) AS questions,
+               JSON_BUILD_OBJECT('answerStatus', ua."answerStatus", 'addedAt', ua."addedAt", 'questionId', gq."questionId") AS answer
+          FROM sql_game g
+          JOIN sql_game_progress fp ON g.id = fp."gameId" AND fp."userId" = 'c9b93818-e50a-42c0-ba27-6af2c6e13c49'
+          JOIN sql_game_progress sp ON g.id = sp."gameId" AND sp."userId" != 'c9b93818-e50a-42c0-ba27-6af2c6e13c49'
+          JOIN sql_game_questions gq ON gq."gameId" = g.id
+          LEFT JOIN sql_questions q ON q.id = gq."questionId"
+          JOIN sql_users fu ON fp."userId" = fu.id
+          JOIN sql_users su ON sp."userId" = su.id
+          LEFT JOIN sql_game_progress gp1 ON fp."userId" = gp1."userId" AND g.id = gp1."gameId"
+          JOIN sql_user_answer ua ON ua."questionId" = gq."questionId" AND ua."gameId" = g.id
     `;
     const result = await this.dataSource.query(query)
 
