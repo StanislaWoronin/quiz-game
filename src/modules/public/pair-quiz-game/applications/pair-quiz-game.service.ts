2ec93ce598821025cddcf70980d4fafc6c73024c
Stanislav <stanislaworonin@yandex.ru>
2023-04-10 15:37:13 +0300

    add logic for and game when one user answered on all questions

--- src/modules/public/pair-quiz-game/applications/pair-quiz-game.service.ts
+++ src/modules/public/pair-quiz-game/applications/pair-quiz-game.service.ts
@@ -63,9 +63,11 @@ export class PairQuizGameService {
 
     if (isLastQuestions) {
       //await this.schedulerRegistry.getTimeout('delayedJob')
-      await this.taskService.handleTimeout(gameId, correctAnswer.questionId)
+      //await this.taskService.handleTimeout(gameId, correctAnswer.questionId)
+      this.schedulerRegistry.getCronJob('')
     }
 
     return await this.gameRepository.sendAnswer(sendAnswerDto);
   }
+
 }
--- src/modules/public/pair-quiz-game/applications/task.service.ts
+++ src/modules/public/pair-quiz-game/applications/task.service.ts
@@ -2,6 +2,7 @@ import {Inject, Injectable} from "@nestjs/common";
 import {Timeout} from "@nestjs/schedule";
 import {IQuizGameQueryRepository} from "../infrastructure/i-quiz-game-query.repository";
 import {IQuizGameRepository} from "../infrastructure/i-quiz-game.repository";
+import {CronJob} from "cron";
 
 @Injectable()
 export class TaskService {
@@ -23,5 +24,18 @@ export class TaskService {
         }
         console.log('task end')
         return
+    }
+
+    addCronJob(gameId: string, userId: string) {
+        const job = new CronJob(`10 * * * * *`, async () => {
+            console.log('start task')
+            const lastUserAnswerProgress = await this.quizGameQueryRepository.currentGameAnswerProgress(userId, gameId)
+
+            if (lastUserAnswerProgress !== 5) {
+                await this.quizGameRepository.forceGameOver(userId, gameId, lastUserAnswerProgress).then()
+            }
+            console.log('task end')
+            return
+        })
     }
 }
